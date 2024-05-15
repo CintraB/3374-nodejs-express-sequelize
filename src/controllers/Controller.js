44dd0bb37ee3a92800893acb6d028c6649a61fa8
Cristhian Cintra <64995266+CintraB@users.noreply.github.com>
2024-05-15 19:02:28 -0300

    Feat: add rotas

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,28 +1,60 @@
 class Controller {
-    constructor(entidadeService){
+    constructor(entidadeService) {
         this.entidadeService = entidadeService;
     }
 
-    async pegaTodos(req,res){
+    async pegaTodos(req, res) {
         try {
             const listaDeRegistros = await this.entidadeService.pegaTodosOsRegistros();
             return res.status(200).json(listaDeRegistros);
         } catch (error) {
-            
+
         }
     }
 
-    async atualiza(req,res){
+    async pegaUmPorId(req, res) {
+        const { id } = req.params;
+        try {
+            const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
+            return res.status(200).json(umRegistro);
+        } catch (error) {
+            // erro
+        }
+    }
+
+    async criaNovo(req, res) {
+        const dadosParaCriacao = req.body;
+        try {
+            const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
+            return res.status(200).json(novoRegistroCriado);
+        } catch (error) {
+            // erro
+        }
+    }
+
+    async atualiza(req, res) {
         const { id } = req.params;
         const dadosAtualizados = req.body;
         try {
-            const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados,Number(id));
-            if(!foiAtualizado){
-                return res.status(400).json({mensagem: 'registro não foi atualizado'});
+            const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
+            if (!foiAtualizado) {
+                return res.status(400).json({ mensagem: 'registro não foi atualizado' });
             }
-            return res.status(200).json({mensagem: 'Atualizado com sucesso'});
+            return res.status(200).json({ mensagem: 'Atualizado com sucesso' });
         } catch (error) {
-            
+
+        }
+    }
+
+    async exclui(req, res) {
+        const { id } = req.params;
+        try {
+            await this.entidadeService.excluiRegistro(Number(id));
+            return res.status(200).json({ mensagem: `id ${id} deletado` });
+
+
+        } catch (error) {
+            return res.status(500).json(error.message);
         }
     }
 }
--- src/services/Services.js
+++ src/services/Services.js
@@ -2,23 +2,34 @@ const { where } = require('sequelize');
 const dataSource = require('../models');
 
 class Services {
-    constructor(nomeDoModel){
+    constructor(nomeDoModel) {
         this.model = nomeDoModel;
     }
 
-    async pegaTodosOsRegistros () {
+    async pegaTodosOsRegistros() {
         return dataSource[this.model].findAll();
-      }
+    }
 
+    async pegaUmRegistroPorId(id) {
+        return dataSource[this.model].findByPk(id);
+    }
 
-    async atualizaRegistro(dadosAtualizados,id){
-        const ListadeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados,{
+    async criaRegistro(dadosDoRegistro) {
+        return dataSource[this.model].create(dadosDoRegistro);
+    }
+
+    async atualizaRegistro(dadosAtualizados, id) {
+        const ListadeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
             where: { id: id }
         });
-        if(ListadeRegistrosAtualizados[0] == 0){
+        if (ListadeRegistrosAtualizados[0] == 0) {
             return false;
         }
         return true;
+    }
+
+    async excluiRegistro(id) {
+        return dataSource[this.model].destroy({ where: { id: id } });
     }
 }
 
