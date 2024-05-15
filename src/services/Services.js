fe40068a3bc9247e71088e085bc1829960c644a4
Cristhian Cintra <64995266+CintraB@users.noreply.github.com>
2024-05-14 23:46:35 -0300

    Feat: PUT
    adicionando método para atualizar.

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize');
 const dataSource = require('../models');
 
 class Services {
@@ -8,6 +9,17 @@ class Services {
     async pegaTodosOsRegistros () {
         return dataSource[this.model].findAll();
       }
+
+
+    async atualizaRegistro(dadosAtualizados,id){
+        const ListadeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados,{
+            where: { id: id }
+        });
+        if(ListadeRegistrosAtualizados[0] == 0){
+            return false;
+        }
+        return true;
+    }
 }
 
 module.exports = Services;
